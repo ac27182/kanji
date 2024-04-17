da1243a447fc37ed719403e49206475b1244e07e
Alex Cameron <alex.cameron@treasure-data.com>
2024-04-17 16:21:33 +0700

    updating generator

--- generate.js
+++ generate.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const kanji = require('./kanji.json')
 
-const FILE = 'partitions.html'
+const JISHO_FILE = 'index.html'
+const SENTANCES_FILE = 'sentances.html'
 
 const base = {
   10: [],
@@ -20,23 +21,41 @@ const base = {
 
 const grouped = kanji
   .reduce((result,item) => {
-    // console.log(result)
     result[item.grade].push(item.kanji)
     return result
   },base)
 
-const all =
-  Object
-    .entries(grouped)
-    .sort((x,y) => Number(y[0]) - Number(x[0]))
-    .map(([grade,kanjis]) => {
+const sorted = Object.entries(grouped).sort((x,y) => Number(y[0]) - Number(x[0]))
 
-      const cells = kanjis.map(kanji => `<a href="https://jisho.org/search/${kanji}%20%23kanji">${kanji}</a>`)
+const jisho = sorted.map(([grade,kanjis]) => {
+    const cells = kanjis.map(kanji => `<a href="https://jisho.org/search/${kanji}%20%23kanji">${kanji}</a>`)
 
-      return `<a href="#grade_${grade}"><div class=section id=grade_${grade}>${grade}</div></a><div class="partition">${cells.join("")}</div>`
-    })
+    return `<a href="#grade_${grade}"><div class=section id=grade_${grade}>${grade}</div></a><div class="partition">${cells.join("")}</div>`
+  })
+  .join("\n")
 
-fs.writeFileSync(
-  FILE,
-  all.join("\n")
-)   
+const sentances = sorted.map(([grade,kanjis]) => {
+    const cells = kanjis.map(kanji => `<a href="https://ac27182.github.io/sentances/compiled/sentances.html?query=${kanji}">${kanji}</a>`)
+
+    return `<a href="#grade_${grade}"><div class=section id=grade_${grade}>${grade}</div></a><div class="partition">${cells.join("")}</div>`
+  })
+  .join("\n")
+
+
+
+const makePage = (partitions) => `
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="./style_kanji.css">
+</head>
+
+<body class="container">${partitions}</body>
+
+</html>
+`
+
+fs.writeFileSync(JISHO_FILE, makePage(jisho))
+
+fs.writeFileSync(SENTANCES_FILE, makePage(sentances))
